974b952ddd7a6a41e6026202cde6687f0103bb2f
mechagdynia2-ai <mechagdynia2@gmail.com>
2025-11-02 22:54:33 +0100

    Add files via upload

---
+++ flutter_webview_apk/lib/main.dart
@@ -0,0 +1,33 @@
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter WebView Demo',
+      home: const WebViewPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class WebViewPage extends StatelessWidget {
+  const WebViewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("WebView App")),
+      body: const WebView(
+        initialUrl: 'https://www.example.com',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
